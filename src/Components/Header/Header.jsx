235b0067abbfbf04aad6689774b7ec8c8d353a61
helloaliimran <hellolaiimran@yahoo.com>
2024-10-15 20:46:55 +0500

    React Essentials

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import recatImg from "../../assets/react-core-concepts.png"
+import "./Header.css"
+
+export default function Header(){
+
+    return (  <header>
+        <img src={recatImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>)
+}
